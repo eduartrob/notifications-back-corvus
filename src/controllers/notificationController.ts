2b5f7d5d57f7a4286f8cb265861309d431dd2308
Sujey Calderon <233291@ids.upchiapas.edu.mx>
2026-06-22 19:04:54 -0600

    Feat: Microservicio de notificaciones finalizada

---
+++ src/controllers/notificationController.ts
@@ -0,0 +1,29 @@
+import type { Request, Response } from 'express';
+import { sendEmail } from '../services/emailService';
+import { sendWhatsAppMessage } from '../services/whatsappService';
+
+export const sendNotification = async (req: Request, res: Response): Promise<any> => {
+    const { type, toEmail, toPhone, subject, message } = req.body;
+
+    if (!message) {
+        return res.status(400).json({ error: 'El campo "message" es requerido.' });
+    }
+
+    let emailSent = false;
+    let whatsappSent = false;
+
+    // Solo ejecuta envío de correo si el tipo lo indica
+    if ((type === 'EMAIL' || type === 'EMAIL_AND_WHATSAPP') && toEmail && subject) {
+        emailSent = await sendEmail(toEmail, subject, message);
+    }
+
+    // Solo ejecuta envío de WhatsApp si el tipo lo indica
+    if ((type === 'WHATSAPP' || type === 'EMAIL_AND_WHATSAPP') && toPhone) {
+        whatsappSent = await sendWhatsAppMessage(toPhone, message);
+    }
+
+    return res.status(200).json({
+        message: 'Proceso de notificación finalizado',
+        status: { emailSent, whatsappSent }
+    });
+};
